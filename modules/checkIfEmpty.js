3705cc00e8271d70d3ac87f33e3baf1715e1b41c
basv1996 <bas.vugts@gmail.com>
2021-11-04 12:47:45 +0100

    added some comments

--- modules/checkIfEmpty.js
+++ modules/checkIfEmpty.js
@@ -1,5 +1,5 @@
-function checkIfEmpty(str) {
-    if(typeof str === 'string' && 
+function checkIfEmpty(str) { 
+    if(typeof str === 'string' && //Als de string een type van string is en het antwoord is minder lang dan 1 character zet dan neer "niet ingevuld"
     str.length < 1 ){
         'Niet ingevuld'
     } else {
