a18e5e57d7ef336405c83937be94786a5e0add35
basv1996 <bas.vugts@gmail.com>
2021-11-04 12:35:29 +0100

    comments added by stringToHex

--- modules/stringToHex.js
+++ modules/stringToHex.js
@@ -1,13 +1,13 @@
 import colors from '../scripts/getColors.js'
 
-function stringToHex(data, key){
-    data.map(obj => {
-        let colorString = obj[key]
-        colors.map(item => {
-            if (item.color === colorString) {
-                return obj[key] = item.hex
+function stringToHex(data, key){  //Deze functie heeft de data en key object als parameter mee
+    data.map(obj => { //Loop door de data objecten heen
+        let colorString = obj[key] //definieren van de variabele colorString
+        colors.map(item => { //Loop door de array heen van colors en zet voor elke kleur gelijk aan "item"
+            if (item.color === colorString) { //Kijk of de naam van de kleur overeen komt met de variable colorString
+                return obj[key] = item.hex //Zo ja geef dan de hexcode terug
             } else {
-                return {[key]: obj[key]}
+                return {[key]: obj[key]} // Zo niet geef dan de kleurnaam terug
             }
         })
     })
